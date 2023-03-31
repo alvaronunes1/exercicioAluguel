ec1e9e035f6e4b169d1b602c2cba94d64d4d6027
vizin <alvaro.nunes15@gmail.com>
2023-03-31 11:14:43 -0300

    feat: botão criado

--- src/Aluguel.java
+++ src/Aluguel.java
@@ -1,18 +1,37 @@
+import javafx.scene.control.Labeled;
+
 import javax.swing.*;
 import javax.swing.text.MaskFormatter;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.text.ParseException;
 
-public class Aluguel {
+public class Aluguel implements ActionListener {
+    private JButton botao;
+
+    public void janela2() {
+        botao.addActionListener(this);
+
+    }
+
+
     public static void main(String[] args) {
 
+
+
+
         JFrame janela = new JFrame("Aluguel de Carros");
-        janela.setSize(600, 400);
+        JButton botao = new JButton("Salvar");
+        janela.setSize(400, 600);
         janela.setVisible(true);
+
+
 
         JPanel painel = new JPanel();
         janela.getContentPane().add(painel);
-
+        janela.getContentPane().add(botao);
+        janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JLabel dadosCliente = new JLabel("Dados do cliente: ");
         JLabel nom = new JLabel("Nome: ");
@@ -27,28 +46,17 @@ public class Aluguel {
 
 
         //DEFINE POSICAO
-        dadosCliente.setBounds(10, 30, 120, 60);
+        dadosCliente.setBounds(10, 30, 120, 40);
         nom.setBounds(30, 60, 100,40);
         cpf.setBounds(35, 80, 100,40);
         tel.setBounds(40, 100, 100,40);
         dadosCarro.setBounds(10, 140, 100, 40);
-        nomCarro.setBounds(30, 160,100,40);
-        placa.setBounds(35,180,100,40);
-        ano.setBounds(40,200,100,40);
-        dtaLocacao.setBounds(45,220,150,40);
-        dtaEntrega.setBounds(50,240,100,40);
+        nomCarro.setBounds(35, 160,100,40);
+        placa.setBounds(40,180,100,40);
+        ano.setBounds(45,200,100,40);
+        dtaLocacao.setBounds(50,220,150,40);
+        dtaEntrega.setBounds(55,240,100,40);
 
-        //DEFINE OS ADD
-        painel.add(dadosCliente);
-        painel.add(nom);
-        painel.add(cpf);
-        painel.add(tel);
-        painel.add(dadosCarro);
-        painel.add(nomCarro);
-        painel.add(placa);
-        painel.add(ano);
-        painel.add(dtaLocacao);
-        painel.add(dtaEntrega);
 
         //DEFINE AS MASCARAS
         MaskFormatter mascaraCpf = null;
@@ -74,8 +82,43 @@ public class Aluguel {
         }
         catch (ParseException excp){
             System.out.println("Erro na formatação: ");
+            System.exit(-1);
         }
 
+        //seta as máscaras nos objetos JFormattedTextField
+        JFormattedTextField jFormattedTextCpf = new JFormattedTextField(mascaraCpf);
+        JFormattedTextField jFormattedTextTel = new JFormattedTextField(mascaraTel);
+        JFormattedTextField jFormattedTextPlaca = new JFormattedTextField(mascaraPlaca);
+        JFormattedTextField jFormattedTextAno = new JFormattedTextField(mascaraAno);
+        JFormattedTextField jFormattedTextDtaLocacao = new JFormattedTextField(mascaraDtaLocacao);
+        JFormattedTextField jFormattedTextDtaEntrega = new JFormattedTextField(mascaraDtaEntrega);
+        jFormattedTextCpf.setBounds(135,80,100,40);
+        jFormattedTextTel.setBounds(140,100,100,40);
+        jFormattedTextPlaca.setBounds(140,180,100,40);
+        jFormattedTextAno.setBounds(145,200,100,40);
+        jFormattedTextDtaLocacao.setBounds(150,220,100,40);
+        jFormattedTextDtaEntrega.setBounds(155,240,100,40);
+
+
+    //DEFINE OS ADD
+        painel.add(dadosCliente);
+        painel.add(nom);
+        painel.add(cpf);
+        painel.add(tel);
+        painel.add(dadosCarro);
+        painel.add(nomCarro);
+        painel.add(placa);
+        painel.add(ano);
+        painel.add(dtaLocacao);
+        painel.add(dtaEntrega);
+        painel.add(jFormattedTextCpf);
+        painel.add(jFormattedTextTel);
+        painel.add(jFormattedTextPlaca);
+        painel.add(jFormattedTextAno);
+        painel.add(jFormattedTextDtaLocacao);
+        painel.add(jFormattedTextDtaEntrega);
+        janela.add(botao);
+        botao.setBounds(200,300,150,80);
 
 
 
@@ -84,7 +127,11 @@ public class Aluguel {
 
 
 
+    }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        botao.setText("Aluguel realizado com sucesso!");
 
     }
 }
---
+++ src/Botao.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//ublic class Botao implements ActionListener {
+    //public static JButton botao;
+
+
+
+
+    //@Override
+   //// public void actionPerformed(ActionEvent e) {
+        //botao.setText("Botão foi clicado");
+
+   // }
+
+//}
